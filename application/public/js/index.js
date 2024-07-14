d083fdca6a00cd83d93a5c4b5a9b43070c547478
Max Cole <MaxIsaiahcole2@gmail.com>
2024-07-13 19:46:11 -0700

    added functionality for index and registration. Moved files to their proper places

---
+++ application/public/js/index.js
@@ -0,0 +1,55 @@
+function updateThumbnailCount(){
+    const count = document.querySelectorAll('.card').length;
+    document.getElementById('counter').textContent = `Cards on screen:${count} `; //+ count;
+}
+
+function buildCardHTML(data){
+
+    return `<div class = "card">
+    <p>${data.title}</p>
+    <img src="${data.url}" alt="product image">
+</div>`;
+}
+
+var result = fetch("https://jsonplaceholder.typicode.com/albums/2/photos")
+    .then(function(resp){
+        //promise chain
+        return resp.json();
+    })
+    .then(function(photo){
+        
+        
+        let main_content = document.getElementById('main-content');
+
+        main_content.innerHTML = ''; //fixed bug that was messing with grid view
+
+        for(let i =  0; i < photo.length;i++){
+            main_content.innerHTML += buildCardHTML(photo[i]);
+        }
+        
+        //sets original thumbnail count
+        updateThumbnailCount();
+
+        //makes array of all thumbnails on screen
+        let cards = document.querySelectorAll('.card');
+        
+    
+
+        //for each that calls a function on each thumbnail, adding a click event listener
+        cards.forEach(function(card) {
+            card.addEventListener('click',function(){
+                this.classList.add('fade-out');
+                setTimeout(() =>{ 
+                    //Arrow notation treats this differently so this breaks if function(){} instead 
+                    // has to do with scope of this being different depending on arrow function
+                    this.remove();
+                    updateThumbnailCount();
+                },500); //remove after 500ms, matches with CSS
+            })
+
+        })
+
+    })
+    .catch(function(error){
+        console.log(error)
+    });
---
+++ application/public/js/registration.js
@@ -0,0 +1,153 @@
+const usernameInput = document.getElementById('username');
+const usernameValidation = document.getElementById('usernameValidation');
+
+const passwordInput = document.getElementById('password');
+const passwordValidation = document.getElementById('passwordValidation');
+
+const confirmPasswordInput = document.getElementById('confirmPassword');
+const confirmPasswordValidation = document.getElementById('confirmPasswordValidation')
+
+const emailInput = document.getElementById('email');
+const emailValidation = document.getElementById('emailValidation');
+
+function countLetters(str){
+    let letterCount = 0; 
+    for(let char of str){
+        if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')){
+            letterCount++;
+        }
+    }
+    return letterCount;
+}
+function startWithLetter(str){
+    if (str.length === 0) return;
+    const firstChar = str[0].toLowerCase();
+    return firstChar >= 'a' && firstChar <= 'z';
+}
+
+function validateUsername(username){
+    let messages = [];
+
+    if(countLetters(username) < 3){
+        messages.push("Username needs to have 3 non-numerical characters");
+    }
+
+    if(!startWithLetter(username)){
+        messages.push("Username must start with a letter");
+    }
+
+    return messages;
+}
+
+function validatePassword(password){
+    let messages = [];
+    if(password.length < 8){
+        messages.push("Password must be at least 8 characters long");
+    }
+
+    let hasNumber = false;
+    let hasUpperCase = false;
+    let hasSpecialChar = false;
+    const specialChars = "/*-+!@#$^&~[]";
+
+    for(let i = 0; i < password.length;i++){
+        let char = password[i];
+        if (char >= '0' && char <= '9'){
+            hasNumber = true;
+        } else if (char === char.toUpperCase() && char !== char.toLowerCase()){
+            hasUpperCase = true;
+        } else if (specialChars.includes(char)){
+            hasSpecialChar = true;
+        }
+
+        if (hasNumber && hasUpperCase && hasSpecialChar) break;
+    }
+
+    if(!hasNumber){
+        messages.push("Password must contain at least one number");
+    }
+
+    if(!hasUpperCase){
+        messages.push("Password must contain one uppercase letter");
+    }
+
+    if(!hasSpecialChar){
+        messages.push("Password must contain one of the following: /*-+!@#$^&~[]")
+    }
+
+    return messages;
+}
+
+function validateEmail(email) {
+    let messages = [];
+    
+
+    if (email.trim() === '') {
+        messages.push("Email is required");
+    }
+    
+
+    return messages;
+}
+
+function checkPasswordMatch(){
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+    const message = "Passwords must match";
+
+    if (password !== confirmPassword) {
+        confirmPasswordValidation.innerHTML = message;
+    } else {
+        confirmPasswordValidation.innerHTML = '';
+    }
+}
+
+document.getElementById('username').addEventListener('input', function(e){
+    const username = e.target.value;
+    console.log("Username: ", username);
+
+    const validationMessages = validateUsername(username);
+    usernameValidation.innerHTML = validationMessages.join('<br>'); 
+});
+
+document.getElementById('password').addEventListener('input', function(e){
+    const password = e.target.value;
+
+    const validationMessages = validatePassword(password);
+    passwordValidation.innerHTML = validationMessages.join('<br>');
+
+    checkPasswordMatch();
+});
+
+document.getElementById('confirmPassword').addEventListener('input', function(e){
+    checkPasswordMatch();
+});
+
+document.getElementById('email').addEventListener('input', function(e) {
+    const email = e.target.value;
+    const validationMessages = validateEmail(email);
+    emailValidation.innerHTML = validationMessages.join('<br>'); 
+});
+
+
+
+function updateButtonState(){
+    const usernameValid = validateUsername(usernameInput.value).length === 0;
+    const passwordValid = validatePassword(passwordInput.value).length === 0;
+    const passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+
+    const registerButton = document.querySelector('button[type = "submit"]');
+
+    if (usernameValid && passwordValid && passwordsMatch){
+        registerButton.removeAttribute('disabled');
+    } else {
+        registerButton.setAttribute('disabled', 'disabled');
+    }
+}
+
+usernameInput.addEventListener('input', updateButtonState);
+passwordInput.addEventListener('input', updateButtonState);
+confirmPasswordInput.addEventListener('input', updateButtonState);
+
+
+updateButtonState();
